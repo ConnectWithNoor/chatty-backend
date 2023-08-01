99fb8b9a7e22f31e3e92f0677859f1c6c737eba1
Noor Muhammad <connectwithnoor1@gmail.com>
2023-08-01 23:25:42 +0500

    added: get user by userid from db method

--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -7,6 +7,17 @@ class UserService {
     await UserModel.create(data);
   }
 
+  public async getUserById(userId: string): Promise<IUserDocument> {
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(userId) } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $project: this.aggregateProject() }
+    ]);
+
+    return users[0];
+  }
+
   public async getUserByAuthId(authId: string): Promise<IUserDocument> {
     const users: IUserDocument[] = await UserModel.aggregate([
       { $match: { authId: new mongoose.Types.ObjectId(authId) } },
